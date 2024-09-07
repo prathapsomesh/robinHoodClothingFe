7c4112c979508ac7c64e3019f52860f2b9be9d34
prathapsomesh <prathap966@gmail.com>
2024-09-07 20:43:32 +0530

    Pratap | updates the title and title bar image

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '@/styles/globals.css';
 import NavbarTop from '@/components/navbar';
 import Bottom from '@/components/Bottom';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Robinhood clothing</title>
+      </Head>
       <NavbarTop />
       <Component {...pageProps} />
       <Bottom />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content="Robinhood Clothing" key="title" />
+        <link rel="icon" href="/headlogo.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
